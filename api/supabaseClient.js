c2747f0b54d9749f651c984b6b369c8f72585ef1
megalester <bairesdev1@gmail.com>
2025-12-09 12:53:19 -0500

    Update supabaseClient.js

--- api/supabaseClient.js
+++ api/supabaseClient.js
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
-
-if (!supabaseUrl || !supabaseKey) {
-  console.error('Missing Supabase environment variables!');
-}
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+);
